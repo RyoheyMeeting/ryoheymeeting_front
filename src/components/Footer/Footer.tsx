759333b30fd3933ee2bd85fcb0f22ede6662a9e0
Takuma.S <baaninreo@gmail.com>
2022-11-06 04:12:16 +0900

    Footerのデザインを実装・偉大なお言葉ページを準備

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
+import { ArrowUp } from "components/icons";
+import { Link } from "react-router-dom";
+import { useTheme } from "styled-components";
+import { FooterStyle } from "./FooterStyle";
 
-type Props = {};
+type Props = {
+  onClickTop?: MouseEventHandler<HTMLButtonElement>;
+};
 
-export const Footer: React.FC<Props> = () => {
+export const Footer: React.FC<Props> = ({ onClickTop }) => {
+  const theme = useTheme();
+
   return (
-    <div>
-      <h1>フッター</h1>
-      <ul>
-        <li>フッター情報</li>
-      </ul>
-    </div>
+    <FooterStyle>
+      <button className="footer_top" onClick={onClickTop}>
+        <ArrowUp fill={theme.global.negative} size="24px" />
+        <span>Top</span>
+      </button>
+      <div className="footer_main">
+        <div className="footer_panel_left">
+          <img src="/img/ryoheykai_icon.png" alt="遼平会アイコン" className="footer_icon" />
+        </div>
+        <div className="footer_panel_center">
+          <div className="footer_links">
+            <Link to="/" className="footer_link">
+              ホーム
+            </Link>
+            <Link to="/shop" className="footer_link">
+              ショップ
+            </Link>
+            <Link to="/grandprixlist" className="footer_link">
+              グランプリ一覧
+            </Link>
+          </div>
+          <div className="footer_links">
+            <Link to="/usertop" className="footer_link">
+              ユーザトップ
+            </Link>
+            <Link to="/portfoliolist" className="footer_link">
+              ポートフォリオ一覧
+            </Link>
+          </div>
+          <div className="footer_links">
+            <Link to="/idaina-okotoba" className="footer_link footer_idai">
+              遼平下からの偉大なお言葉
+            </Link>
+          </div>
+        </div>
+        <div className="footer_panel_right">
+          <img src="/img/collvo_teamdisk.png" alt="COLLVOチームディスク" className="footer_disk" />
+        </div>
+      </div>
+      <span className="footer_copyright">Copyright by COLLVO</span>
+    </FooterStyle>
   );
 };
---
+++ src/pages/IdaiNaOkotoba/IdaiNaOkotoba.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { WithHeaderFooter } from "components/Layout/WithHeaderFooter/WithHeaderFooter";
+
+type Props = {};
+
+export const IdaiNaOkotoba: React.FC<Props> = () => {
+  return (
+    <WithHeaderFooter>
+      <h1>遼平下からの偉大なお言葉</h1>
+      <ul>
+        <li>準備中</li>
+      </ul>
+    </WithHeaderFooter>
+  );
+};
