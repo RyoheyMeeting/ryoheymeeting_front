f7ffb137c99f548429a668569b6d896bf0de14ec
Takuma.S <baaninreo@gmail.com>
2022-11-06 00:35:56 +0900

    Buttonのデザインを実装

--- src/Types/Utils.ts
+++ src/Types/Utils.ts
@@ -1,3 +1,6 @@
+import { ComponentType } from "react";
+import { IconProps } from "components/icons/components/IconPrototype/IconPrototype";
+
 export type Dict<T> = {
   [key: string]: T;
 };
@@ -6,3 +9,5 @@ export type ButtonOpts = {
   disabled?: boolean;
   handler?: () => void;
 };
+
+export type Icon = ComponentType<IconProps>;
