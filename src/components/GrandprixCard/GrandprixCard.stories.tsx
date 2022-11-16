cb2d14b42beea73a687035e35a8371139782960d
Takuma.S <baaninreo@gmail.com>
2022-11-16 21:58:26 +0900

    GrandPrixCardを実装

---
+++ src/components/GrandprixCard/GrandprixCard.stories.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { GrandprixCard } from "./GrandprixCard";
+import { GrandPrixStatus } from "services/GrandPrixes/GrandPrixes";
+
+export default {
+  title: "components/GrandprixCard",
+  component: GrandprixCard,
+} as ComponentMeta<typeof GrandprixCard>;
+
+const Template: ComponentStory<typeof GrandprixCard> = (args) => <GrandprixCard {...args} />;
+
+export const Yet = Template.bind({});
+Yet.args = {
+  title: "第１回　遼平会",
+  subTitle: "逃げるは恥だが遼平は恥じゃない",
+  date: new Date(),
+  status: GrandPrixStatus.yet
+};
+
+export const Doing = Template.bind({});
+Doing.args = {
+  title: "第１回　遼平会",
+  subTitle: "逃げるは恥だが遼平は恥じゃない",
+  date: new Date(),
+  status: GrandPrixStatus.doing
+};
+
+export const Done = Template.bind({});
+Done.args = {
+  title: "第１回　遼平会",
+  subTitle: "逃げるは恥だが遼平は恥じゃない",
+  date: new Date(),
+  status: GrandPrixStatus.done
+};
---
+++ src/components/GrandprixCard/GrandprixCard.tsx
@@ -0,0 +1,31 @@
+import React, { useMemo } from "react";
+import { GrandPrix } from "services/GrandPrixes/GrandPrixes";
+import { dateToFormat } from "Utils/funcs";
+import { GrandprixStatusTag } from "./components/GrandprixStatus/GrandprixStatusTag";
+import { GrandprixCardStyle, IconFullStyle } from "./GrandprixCardStyle";
+
+type Props = {
+  title: string;
+  subTitle: string;
+  date: Date;
+  status: GrandPrix["status"];
+};
+
+export const GrandprixCard: React.FC<Props> = ({ title, subTitle, date, status }) => {
+  const dateStr = useMemo(() => dateToFormat(date, "Y/M/D"), [date]);
+
+  return (
+    <GrandprixCardStyle status={status}>
+      <div className="grandprixcard_logo">
+        <IconFullStyle src="/img/logo_full.svg" wrapper="svg" />
+      </div>
+      <div className="grandprixcard_main">
+        <span className="grandprixcard_title">{title}</span>
+        <span className="grandprixcard_subtitle">{subTitle}</span>
+        <span className="grandprixcard_date">{dateStr} 開催</span>
+      </div>
+      <GrandprixStatusTag status={status} />
+      <div className="grandprixcard_border" />
+    </GrandprixCardStyle>
+  );
+};
